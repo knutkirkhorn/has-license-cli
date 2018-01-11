caea8689a2809afbab5d89bdaee7e294387f1014
Knut Kirkhorn <knutkirk@hotmail.com>
2018-01-11 20:15:12 +0100

    Add module files

---
+++ cli.js
@@ -0,0 +1,24 @@
+const meow = require('meow');
+const hasLicense = require('has-license');
+
+const cli = meow(`
+        Usage
+          $ has-license <path>
+
+        Examples
+          $ has-license
+          $ has-license https://github.com/Knutakir/has-license
+          $ has-license C:\Users\Knut\Documents\GitHub\has-license 
+`);
+
+const path = cli.input[0];
+
+if (path && cli.input.length === 1) {
+    hasLicense(path).then(result => {
+        console.log(result);
+    });
+} else {
+    hasLicense('.').then(result => {
+        console.log(result);
+    });
+}
